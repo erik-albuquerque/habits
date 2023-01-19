58c4b2701245eefa926a611ff83a8ae9eccf8b2e
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-01-19 19:10:10 -0300

    feat(server): add summary days route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -144,4 +144,39 @@ export async function appRoutes(app: FastifyInstance) {
       throw new Error("Error on complete day!");
     }
   });
+
+  app.get("/summary", async (_, response) => {
+    try {
+      const summary = await prisma.$queryRaw`
+        SELECT
+          D.id,
+          D.date,
+          (
+            SELECT 
+              cast(count(*) as float)
+            FROM day_habits DH
+            WHERE DH.day_id = D.id
+          ) as completed,
+          (
+            SELECT 
+              cast(count(*) as float)
+            FROM habit_week_days HWD
+            JOIN habits H
+              ON H.id = HWD.habit_id
+            WHERE
+              HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+              AND H.created_at <= D.date
+          ) as amount
+        FROM days D
+    `;
+
+      response.status(200).send({ summary });
+    } catch (error) {
+      console.error(error);
+
+      response.status(400).send({ error: "Error on get summary!" });
+
+      throw new Error("Error on get summary!");
+    }
+  });
 }
