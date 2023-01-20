762690e398dd3c1963c635e0563157078110eb1c
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-01-19 23:23:08 -0300

    feat(mobile): add Habit screen

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Habit } from "../Screens/Habit";
 import { Home } from "../Screens/Home";
 import { New } from "../Screens/New";
 
@@ -8,6 +9,8 @@ export function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
+
+      <Screen name="habit" component={Habit} />
 
       <Screen name="new" component={New} />
     </Navigator>
