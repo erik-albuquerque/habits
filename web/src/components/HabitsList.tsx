80c07b3fb8c49302acb94e7c8585e63bffd9bde1
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-01-25 15:08:05 -0300

    feat(web): synchronizing completed habits

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -5,6 +5,7 @@ import { Checkbox } from "./Checkbox";
 
 interface HabitsList {
   date: Date;
+  onCompletedChanged: (completed: number) => void;
 }
 
 interface HabitsInfo {
@@ -16,8 +17,34 @@ interface HabitsInfo {
   completedHabits: string[];
 }
 
-export function HabitsList({ date }: HabitsList) {
+export function HabitsList({ date, onCompletedChanged }: HabitsList) {
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
+
+  const isDateInPast = dayjs(date).endOf("day").isBefore(new Date());
+
+  async function handleToggleHabit(habitId: string) {
+    await api.patch(`/habits/${habitId}/toggle`);
+
+    const isHabitAlreadyCompleted =
+      habitsInfo!.completedHabits.includes(habitId);
+
+    let completedHabits: string[] = [];
+
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(
+        (id) => id !== habitId
+      );
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId];
+    }
+
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits,
+    });
+
+    onCompletedChanged(completedHabits.length);
+  }
 
   useEffect(() => {
     api
@@ -31,8 +58,6 @@ export function HabitsList({ date }: HabitsList) {
       });
   }, []);
 
-  const isDateInPast = dayjs(date).endOf("day").isBefore(new Date());
-
   return (
     <div className="mt-6 flex flex-col gap-3">
       {habitsInfo?.possibleHabits.map((habit) => {
@@ -42,6 +67,7 @@ export function HabitsList({ date }: HabitsList) {
             title={habit.title}
             checked={habitsInfo.completedHabits.includes(habit.id)}
             disabled={isDateInPast}
+            onCheckedChange={() => handleToggleHabit(habit.id)}
             className="font-semibold text-xl group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400"
           />
         );
